4067875026de091af4428d9e2a3841cceb6bcbe8
Jesus Rincon <jesus.rincon@elliemae.com>
2020-07-13 14:01:35 -0400

    Remove package.lock and more unnecesary code

--- core/components/navbar/index.js
+++ core/components/navbar/index.js
@@ -24,28 +24,6 @@ const WebNavbar = () => {
           ))}
         </Flex>
       )}
-
-      {/* <Box as="button" display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
-        <svg color="white" fill="white" width="12px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </Box>
-      {show ? (
-
-      ) : (
-        <Box
-          display={{ sm: show ? 'block' : 'none', md: 'flex' }}
-          width={{ sm: 'full', md: 'auto' }}
-          alignItems="center"
-          justifyContent="flex-end"
-          flexGrow={1}
-        >
-          {webNavbarAuthenticated.map((item) => (
-            <MenuItems item={item} />
-          ))}
-        </Box>
-      )} */}
     </>
   );
 
