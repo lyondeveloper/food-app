7368de8a08800e6e2156ed1e4cdf607f7d947d33
Jesus Rincon <jesus.rincon@elliemae.com>
2020-07-13 13:59:20 -0400

    Removed unnecesary code

--- core/components/landing-page/index.js
+++ core/components/landing-page/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-// import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
 const LandingPage = () => {
-  // With this approach,
-  // We don't need connect, mapDispatchToState, mapDispatchToProps
-  // const dispatch = useDispatch();
-  // const onClick = () => dispatch(counterActions.increment());
-  // const { user } = useSelector((state) => state, shallowEqual);
-
   return <div>landing page</div>;
 };
 
